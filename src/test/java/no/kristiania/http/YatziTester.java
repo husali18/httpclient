b40372a1af5595fb08d8eb78835af21766c242a8
Ali;) <ali45v@gmail.com>
2019-09-11 17:19:04 +0200

    Initial commit

---
+++ src/test/java/no/kristiania/http/YatziTester.java
@@ -0,0 +1,14 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class YatziTester {
+
+    @Test
+    void YatziTest(){
+        assertEquals(4, 2+2);
+    }
+
+}

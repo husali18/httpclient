374468604ab45f71c47cb3eb5e8cc67251e0e503
Ali;) <ali45v@gmail.com>
2019-09-18 17:26:38 +0200

    Add .travis.yml

--- src/test/java/no/kristiania/http/YatziTester.java
+++ src/test/java/no/kristiania/http/YatziTester.java
@@ -7,8 +7,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class YatziTester {
 
     @Test
-    void YatziTest(){
-        assertEquals(4, 2+2);
+    void shouldCalculateForOnes() {
+        assertEquals(0, score("ONES", new int[] { 2, 3, 4, 5, 6 }));
+        assertEquals(3, score("ONES", new int[] { 2, 1, 4, 1, 1 }));
+        assertEquals(5, score("ONES", new int[] { 1, 1, 1, 1, 1 }));
+    }
+
+    private int score(String ones, int[] randomOutcome) {
+        int amountsOfOnes = 0;
+        for (int i = 0; i < randomOutcome.length; i++){
+            if (randomOutcome[i] == 1){
+                amountsOfOnes = amountsOfOnes + randomOutcome[i];
+            }
+        }
+        return amountsOfOnes;
+    }
+
+    public static void main(String args[]){
+
+        System.out.println();
     }
 
 }
+
